93cd75b20cf4049fe51ac96aeda7e99fd04e21d0
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-11-01 18:12:46 +0530

    Add rectifier programs

---
+++ Matlab/full_wave_rectifier.m
@@ -0,0 +1,24 @@
+﻿% Full Wave Rectifier Program
+t=(0:1/10000:0.04);
+f=input('Enter frequency(in Hz) :')
+A=input('Enter Amplitude(in V) :')
+% sig denotes signal
+sig=A*sin(2*pi*f*t); % sin wave of f frequency and A v amplitude
+subplot(211)
+plot(sig);
+grid
+
+u=1:9;
+for l=1:400 % checking for all 0.04*10000 samples
+%    
+if sin(2*pi*f*t(l))<=0
+    sig(l)= -A*sin(2*pi*f*t(l));
+else
+    sig(l) = A*sin(2*pi*f*t(l));
+
+end
+end
+subplot(212)
+plot(sig);
+grid
+
---
+++ Matlab/half_wave_rectifier.m
@@ -0,0 +1,24 @@
+﻿% Half Wave Rectifier Program
+l=linspace(0,10,100);
+f=input('Enter frequency(in Hz) :')
+A=input('Enter Amplitude(in V) :')
+sig=A*sin(2*pi*f*l); % sig denotes signal
+subplot(211)
+plot(sig);
+grid
+
+% u=1:9;
+% t=(1:)
+for t=1:100
+   
+if A*sin(2*pi*f*l(t))<=0
+    sig(t)=0;
+else
+    sig(t) = A*sin(2*pi*f*l(t));
+
+end
+end
+subplot(212)
+plot(sig);
+grid
+
