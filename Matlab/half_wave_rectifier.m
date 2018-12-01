be20def56e68678584e5be21528b2d1cf4d0befb
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-02 00:05:13 +0530

    Fix comment syntax errors for use in octave

--- Matlab/half_wave_rectifier.m
+++ Matlab/half_wave_rectifier.m
@@ -1,14 +1,14 @@
-﻿% Half Wave Rectifier Program
+% Half Wave Rectifier Program %
 l=linspace(0,10,100);
 f=input('Enter frequency(in Hz) :')
 A=input('Enter Amplitude(in V) :')
-sig=A*sin(2*pi*f*l); % sig denotes signal
+sig=A*sin(2*pi*f*l); % sig denotes signal %
 subplot(211)
 plot(sig);
 grid
 
-% u=1:9;
-% t=(1:)
+% u=1:9; %
+% t=(1:) %
 for t=1:100
    
 if A*sin(2*pi*f*l(t))<=0
